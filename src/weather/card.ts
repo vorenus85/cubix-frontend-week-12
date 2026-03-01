92d92e0d1312b8d342182d1ab19e83eba8481ad5
János Perge <janos.perge@gmail.com>
2026-03-01 22:29:50 +0100

    add delete card event

--- src/weather/card.ts
+++ src/weather/card.ts
@@ -13,8 +13,24 @@ export class WeatherCard {
               <h4>${city} - ${date}</h4>
               <p>${minTemp} °C</p>
               <p>${maxTemp} °C</p>
+              <button class="weather-card-delete btn" type="button">Delete</button>
             </div>
           </div>`,
     );
+
+    this.deleteCard();
+  }
+
+  private deleteCard() {
+    document.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+
+      if (target.classList.contains('weather-card-delete')) {
+        const card = target.closest('.weather-card');
+        if (card) {
+          card.remove();
+        }
+      }
+    });
   }
 }
